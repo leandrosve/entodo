eeecf5cceb85966624492122a0fa7264280691c0
Leandro <leandrosvetlich@gmail.com>
2021-02-09 17:48:35 -0300

    initial commit

---
+++ backend/src/main/java/com/leandrosve/entodo/EntodoApplication.java
@@ -0,0 +1,13 @@
+package com.leandrosve.entodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntodoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/leandrosve/entodo/EntodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandrosve.entodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
